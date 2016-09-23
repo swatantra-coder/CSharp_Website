e9a98c242389e83a99c5d8a5b0bace9b6bd6811a
Xavier DONO <xavier.dono@gmail.com>
2016-09-23 21:15:40 +0200

    Mise en place des menus

---
+++ WebApplication1/Controllers/MeetingController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MeetingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
