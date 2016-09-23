4619185955a698436b1413d0ac80cb1dca9edc9f
Xavier DONO <xavier.dono@gmail.com>
2016-09-23 20:40:27 +0200

    Ajoutez des fichiers projet.

---
+++ WebApplication1/Controllers/OffreController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class OffreController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
